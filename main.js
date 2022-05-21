3628fad6a81b8b1ca0fe1add943c3d5cdedf5a1a
Emanuelvtsg <evtsg@cin.ufpe.br>
2022-05-20 21:26:33 -0300

    falta desenhar curva

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function Interpolação(pontoA, pontoB, t){
 function deCasteljau(pontos, t){
     var grau = pontos.length -1;
     if (grau === 1){
-        return Interpolação(pontos[0], pontos[1], t)
+        return Interpolação(pontos[0], pontos[1], t);
     }
     else {
         var pontosIntermed = [];
@@ -38,19 +38,23 @@ var addPonto = document.getElementById("addPonto");
 var apagarPonto = document.getElementById("apagarPonto");
 var proxPonto = document.getElementById("proxPonto");
 var antPonto = document.getElementById("antPonto");
-var exibirPontos = document.getElementById("exibirPontos");
-var exibirPoligonais = document.getElementById("exibirPoligonais");
-var exibirCurvas = document.getElementById("exibirCurvas");
+var exibirPontosCheckbox = document.getElementById("exibirPontos");
+var exibirPoligonaisCheckbox = document.getElementById("exibirPoligonais");
+var exibirCurvasCheckbox = document.getElementById("exibirCurvas");
 var textoApoio = document.getElementById("textoApoio");
 var canvas = document.getElementById("canvas");
+
+// canvas
 var ctx = canvas.getContext("2d");
 
 // variáveis auxiliares
 
-const raioPonto = 6;
-var curvas = [] // armazenar ponteiros para as curvas
-var curvaSelecionada = -1;
-var pontoSelecionado = -1;
+const raioPonto = 4;
+var curvas = []; // array de curvas, onde cada curva é um array de pontos
+curvas.push([]);
+var curvaSelecionada = -1; // ponteiro para a curva selecionada
+var pontoSelecionado = []; // array de ponteiro para as curvas, a posição 0 indica o ponto selecionado para a curva 0, etc
+pontoSelecionado.push(0);
 var numeroAvaliacoes = 100;
 var estadoCanvas = 0; // 1 - está acrescentando, 2 - está reposicionando
 var clique = false;
@@ -70,24 +74,28 @@ function desenharLinha(pontoA, pontoB){
     ctx.beginPath();
     ctx.lineTo(pontoA.coordX, pontoA.coordY);
     ctx.lineTo(pontoB.coordX, pontoB.coordY);
+    ctx.strokeStyle = "5px"
     ctx.stroke();
 }
 
 function desenharPoligonoControle(pontos){
-    for (let i = 0; i < pontos.length; i++){
+    for (let i = 0; i < pontos.length - 1; i++){
         desenharLinha(pontos[i], pontos[i+1]);
     }
 }
 
+// para desenhar a curva de bezier, adiciona o primeiro e o ultimo pontos de controle ao array, e pega os outros 
+// através do deCasteljau, utilizando o numero de avaliaçoes para calcular quantos serão (são (nAval - 2) pontos interpolados)
+// depois, chama a funçao de desenhar poligono para ligar cada um desses pontos, o que vai ficar parecido com uma curva
 function desenharCurvaBezier(pontos) {
     if(pontos.length > 2){
         var curvasBezier = [];
         curvasBezier.push(pontos[0]);
-        for (let i = 0; i < numeroAvaliacoes-2; i++){
+        for (let i = 1; i <= numeroAvaliacoes-2; i++){
             curvasBezier.push(deCasteljau(pontos, i/(numeroAvaliacoes)));
         }
         curvasBezier.push(pontos[pontos.length-1]);
-        desenharCurvaBezier(curvasBezier);
+        desenharPoligonoControle(curvasBezier);
     }
 }
 
@@ -97,18 +105,40 @@ function redesenhar() {
     if (exibirPontos){
         for (let i = 0; i < curvas.length; i++){
             for (let j = 0; j < curvas[i].length; j++){
+                if ((j == pontoSelecionado[curvaSelecionada]) && (i == curvaSelecionada)){
+                    ctx.strokeStyle = "black";
+                }
+                else {
+                    ctx.strokeStyle = "black";
+                }
+                
                 desenharPonto(curvas[i][j]);
+
             }
         }
     }
     if (exibirPoligonais){
         for (let i = 0; i < curvas.length; i++){
+            if (i == curvaSelecionada){
+                ctx.strokeStyle = "black";
+            }
+            else {
+                ctx.strokeStyle = "black";
+            }
             desenharPoligonoControle(curvas[i]);
         }
     }
     if (exibirCurvas && numeroAvaliacoes > 1){
         for (let i = 0; i < curvas.length; i++){
+            if (i == curvaSelecionada) {
+                ctx.strokeStyle = "black";
+            }
+            else {
+                ctx.strokeStyle = "black";
+            }
             desenharCurvaBezier(curvas[i]);
+            
+            
         }
     }
 
@@ -120,10 +150,14 @@ function redesenhar() {
 criarCurva.addEventListener("click", function(event) {
     if(curvaSelecionada === -1 || curvas[curvaSelecionada]?.length > 1){
         estadoCanvas = 1;
-        const novaCurva = [];
+        var novaCurva = [];
         curvas.push(novaCurva);
         pontoSelecionado.push(0);
         curvaSelecionada++;
+        textoApoio.innerText = "Clique na tela para adicionar os pontos para a sua curva.";
+    }
+    else {
+        textoApoio.innerText = "Finalize a curva atual antes de adicionar uma nova.";
     }
 
 });
@@ -163,9 +197,9 @@ addPonto.addEventListener("click", function(event){
 apagarPonto.addEventListener("click", function(event){
     if(curvas[curvaSelecionada].length > 0){
         curvas[curvaSelecionada].splice(pontoSelecionado[curvaSelecionada], 1);
-        if(curvas[curvaSelecionada].length === 0){
+         pontoSelecionado.splice(curvaSelecionada, 1);
+        if(curvas[curvaSelecionada].length === 0){ // se apagamos o ultimo elemento
             curvas.splice(curvaSelecionada, 1);
-            pontoSelecionado.splice(curvaSelecionada, 1);
             if(curvaSelecionada > 0){
                 curvaSelecionada--;
             }
@@ -193,17 +227,17 @@ antPonto.addEventListener("click", function(event){
 
 });
 
-exibirPontos.addEventListener("click", function(event){
+exibirPontosCheckbox.addEventListener("click", function(event){
     exibirPontos = !exibirPontos;
     redesenhar();
 });
 
-exibirPoligonais.addEventListener("click", function(event){
+exibirPoligonaisCheckbox.addEventListener("click", function(event){
     exibirPoligonais = !exibirPoligonais;
     redesenhar();
 });
 
-exibirCurvas.addEventListener("click", function(event){
+exibirCurvasCheckbox.addEventListener("click", function(event){
     exibirCurvas = !exibirCurvas;
     redesenhar();
 });
@@ -212,20 +246,37 @@ exibirCurvas.addEventListener("click", function(event){
 
 // para o canvas
 canvas.addEventListener("mousedown", function(event){
-
+    clique = true;
+    var pontoA = new Ponto(event.offsetX, event.offsetY);
+    if (estadoCanvas == 1){
+        curvas[curvaSelecionada].push(pontoA);
+    }
+    else if (estadoCanvas == 2){
+        curvas[curvaSelecionada].splice(pontoSelecionado[curvaSelecionada], 1, pontoA);
+    }
+    redesenhar();
 });
 
 canvas.addEventListener("mousemove", function(event){
-
+    if (clique) {
+        if (estadoCanvas == 2) {
+            var pontoA = new Ponto(event.offsetX, event.offsetY);
+            curvas[curvaSelecionada]?.splice(pontoSelecionado[curvaSelecionada], 1, pontoA);
+        }
+            redesenhar();
+        }
 });
 
 canvas.addEventListener("mouseup", function(event){
-
+    clique = false;
+    redesenhar();
 });
 
 
 
 // para o campo avaliações -> teclado
 campoAvaliacoes.addEventListener("keyup", function(event){
-
+    var entrada = event.target.value;
+    numeroAvaliacoes = parseInt(entrada);
+    redesenhar();
 });
